6ee43ec505ff88d84418fdb9c72b0e0b8130a064
WendyW <wwang184@fordham.edu>
2020-05-05 09:00:37 -0400

    Update index.js
    import dependency mdbreact

--- luxury-react/src/index.js
+++ luxury-react/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 
 
 ReactDOM.render(
